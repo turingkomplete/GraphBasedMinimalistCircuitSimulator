8120a03a2126a14af6d867f2ff202cab09fcacb5
turingkomplete <turingkomplete@gmail.com>
2020-06-08 12:13:11 +0430

    Modify Main.java

--- src/simulator/Main.java
+++ src/simulator/Main.java
@@ -2,7 +2,9 @@
 
 package simulator;
 
+import simulator.control.Circuit;
 import simulator.control.Simulator;
+import simulator.gates.combinational.And;
 import simulator.gates.sequential.Clock;
 import simulator.gates.combinational.ExplicitInput;
 import simulator.gates.sequential.flipflops.DFlipFlop;
@@ -10,16 +12,18 @@ import simulator.gates.sequential.flipflops.DFlipFlop;
 public class Main {
 
     public static void main(String[] args) {
-        Clock clock = new Clock("CLOCK", 1000);
-        ExplicitInput trueInput = new ExplicitInput("TRUE", true);
-        DFlipFlop dFlipFlop1 = new DFlipFlop("D1", clock.getOutput(0), trueInput.getOutput(0));
-        DFlipFlop dFlipFlop2 = new DFlipFlop("D2", clock.getOutput(0), dFlipFlop1.getOutput(0));
+        ExplicitInput trueValue = new ExplicitInput("TRUE", true);
+        ExplicitInput falseValue = new ExplicitInput("FALSE", false);
+        And a1 = new And("AND1", trueValue.getOutput(0), trueValue.getOutput(0));
+        And a2 = new And("AND2", trueValue.getOutput(0), a1.getOutput(0));
+        And a3 = new And("AND3", trueValue.getOutput(0), a2.getOutput(0));
+        a1.addInput(a3.getOutput(0));
 
         Simulator.circuit.startCircuit();
 
         //debug code
         while (true) {
-            System.out.println(dFlipFlop1.getOutput(0).getValue() + "   " + dFlipFlop2.getOutput(0).getValue());
+            System.out.println(a3.getOutput(0).getValue());
             try {
                 Thread.sleep(0);
             } catch (Exception e) {
