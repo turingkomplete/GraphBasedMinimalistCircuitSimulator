428bf87ea803896f6c32a4065e2823130b7d8317
turingkomplete <turingkomplete@gmail.com>
2020-06-09 10:42:57 +0430

    Add Memory.java

---
+++ src/simulator/gates/combinational/Memory.java
@@ -0,0 +1,51 @@
+package simulator.gates.combinational;
+
+import simulator.network.Link;
+import simulator.network.Node;
+
+public class Memory extends Node {
+    private Boolean[] memory;
+
+    public Memory(String label, Link... links) {
+        super(label, links);
+        memory = new Boolean[65536];
+        for (int i = 0; i < 32; ++i) {
+            addOutputLink(false);
+        }
+    }
+
+    private int address() {
+        int temp = 0;
+        for (int i = 1; i <= 16; ++i) {
+            if(inputs.size() > i) {
+                if(getInput(i).getValue()) {
+                    temp += Math.pow(2, 16 - i);
+                }
+            }
+        }
+        return temp;
+    }
+
+    private void memoryWrite() {
+        for(int i = 17; i <= 48; ++i) {
+            memory[address() + i - 17] = getInput(i).getValue();
+        }
+    }
+
+    private void memoryRead(){
+        for (int i = 0; i < 32; ++i) {
+            getOutput(i).setValue(memory[address() + i]);
+        }
+    }
+
+    @Override
+    public void evaluate() {
+        if(getInputs().size() >= 49) {
+            if (getInput(0).getValue()) {
+                memoryWrite();
+            } else {
+                memoryRead();
+            }
+        }
+    }
+}
