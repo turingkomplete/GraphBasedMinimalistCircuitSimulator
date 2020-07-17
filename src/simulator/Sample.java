4734739b4fa3f8a9fb2137d70d7a4c0019b739af
turingkomplete <turingkomplete@gmail.com>
2020-07-17 16:16:01 +0430

    Add multiplexer sample

--- src/simulator/Sample.java
+++ src/simulator/Sample.java
@@ -12,7 +12,10 @@ public class Sample {
                 Simulator.falseLogic, Simulator.trueLogic, Simulator.trueLogic, Simulator.falseLogic, Simulator.trueLogic,
                 Simulator.falseLogic, Simulator.trueLogic, Simulator.falseLogic, Simulator.falseLogic, Simulator.trueLogic);
 
-        Simulator.debugger.addTrackItem(adder);
+        Mux4x1 mux = new Mux4x1("MUX", "6X1", Simulator.falseLogic, Simulator.trueLogic,
+                Simulator.falseLogic, Simulator.trueLogic, Simulator.trueLogic, Simulator.falseLogic);
+
+        Simulator.debugger.addTrackItem(adder, mux);
         Simulator.debugger.setDelay(200);
         Simulator.circuit.startCircuit();
     }
